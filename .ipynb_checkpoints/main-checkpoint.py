2baeb5daf318ad02d1bd7b110771b650344ff5bd
Andy Wang <andywang0321@gmail.com>
2023-02-01 19:30:25 -0800

    new ipynb file for data exploration

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,5 +1,5 @@
 import os
-import torch
+
 import constants
 from data.StartingDataset import StartingDataset
 from networks.StartingNetwork import StartingNetwork
@@ -11,13 +11,13 @@ def main():
     hyperparameters = {"epochs": constants.EPOCHS, "batch_size": constants.BATCH_SIZE}
 
     # TODO: Add GPU support. This line of code might be helpful.
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     print("Epochs:", constants.EPOCHS)
     print("Batch size:", constants.BATCH_SIZE)
 
-    # Initalize dataset and model. Then train the model!pytho
-    data_path = "data_sets/train.csv" #TODO: make sure you have train.csv downloaded in your project! this assumes it is in the project's root directory (ie the same directory as main) but you can change this as you please
+    # Initalize dataset and model. Then train the model!
+    data_path = "train.csv" #TODO: make sure you have train.csv downloaded in your project! this assumes it is in the project's root directory (ie the same directory as main) but you can change this as you please
 
     train_dataset = StartingDataset(data_path)
     val_dataset = StartingDataset(data_path)
--- train_functions/.ipynb_checkpoints/starting_train-checkpoint.py
+++ train_functions/.ipynb_checkpoints/starting_train-checkpoint.py
@@ -38,7 +38,6 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
         # Loop over each batch in the dataset
         for batch in tqdm(train_loader):
             # TODO: Forward propagate
-            
 
             # TODO: Backpropagation and gradient descent
 
