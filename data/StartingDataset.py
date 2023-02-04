added forward/backward prop, gradient descent, fixed dimension and dtype issues

data/StartingDataset.py
@@ -16,7 +16,7 @@ class StartingDataset(torch.utils.data.Dataset):
 
         # Preprocess the data. These are just library function calls so it's here for you
         self.df = pd.read_csv(data_path)
-        self.vectorizer = CountVectorizer(stop_words='english', max_df=0.99, min_df=0.005)
+        self.vectorizer = CountVectorizer(stop_words = 'english', max_df = 0.99, min_df = 0.005)
         self.sequences = self.vectorizer.fit_transform(self.df.question_text.tolist()) # matrix of word counts for each sample
         self.labels = self.df.target.tolist() # list of labels
         self.token2idx = self.vectorizer.vocabulary_ # dictionary converting words to their counts
@@ -28,7 +28,7 @@ class StartingDataset(torch.utils.data.Dataset):
         i (int): the desired instance of the dataset
         '''
         # return the ith sample's list of word counts and label
-        return self.sequences[i, :].toarray(), self.labels[i]
+        return torch.as_tensor(self.sequences[i, :].toarray()), self.labels[i]
 
     # TODO: return the size of the dataset
     def __len__(self):

networks/StartingNetwork.py
@@ -9,17 +9,18 @@ class StartingNetwork(torch.nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(12122002, 50) # What could that number mean!?!?!? Ask an officer to find out :)
-        self.fc2 = nn.Linear(50, 10)
-        self.sigmoid = nn.Sigmoid()
+        self.fc1 = nn.Linear(110, 50) # 110 is the length of vocabulary
+        self.fc2 = nn.Linear(50, 2)
+        self.ReLU = nn.ReLU()
 
     def forward(self, x):
         '''
         x (tensor): the input to the model
         '''
+        x = x.squeeze(1).float()
         x = self.fc1(x)
+        x = self.ReLU(x)
         x = self.fc2(x)
-        x = self.sigmoid(x)
         return x
 
 

train_functions/starting_train.py
@@ -21,10 +21,10 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
 
     # Initialize dataloaders
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True
+        train_dataset, batch_size = batch_size, shuffle = True
     )
     val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=batch_size, shuffle=True
+        val_dataset, batch_size = batch_size, shuffle = True
     )
 
     # Initalize optimizer (for gradient descent) and loss function
@@ -37,21 +37,27 @@ def starting_train(train_dataset, val_dataset, model, hyperparameters, n_eval):
 
         # Loop over each batch in the dataset
         for batch in tqdm(train_loader):
-            # TODO: Forward propagate
-            
+            inputs, labels = batch
+            outputs = model(inputs)
 
-            # TODO: Backpropagation and gradient descent
+            # Backprop
+            loss = loss_fn(outputs, labels)
+            loss.backward()       # Compute gradients
+            optimizer.step()      # Update all the weights with the gradients you just calculated
+            optimizer.zero_grad() # Clear gradients before next iteration
 
             # Periodically evaluate our model + log to Tensorboard
             if step % n_eval == 0:
                 # TODO:
                 # Compute training loss and accuracy.
                 # Log the results to Tensorboard.
+                print('Epoch:', epoch, 'Loss:', loss.item())
 
                 # TODO:
                 # Compute validation loss and accuracy.
                 # Log the results to Tensorboard. 
                 # Don't forget to turn off gradient calculations!
+                
                 evaluate(val_loader, model, loss_fn)
 
             step += 1
@@ -82,4 +88,16 @@ def evaluate(val_loader, model, loss_fn):
 
     TODO!
     """
-    pass
+    model.eval()
+
+    with torch.no_grad(): # IMPORTANT: turn off gradient computations
+        for batch in tqdm(val_loader):
+            inputs, labels = batch
+            outputs = model(inputs)
+            prediction = torch.argmax(outputs)
+    
+    #TODO: right now the predictions are integers like 2, 32, 26, etc. shouldn't it be 0 or 1?
+    # Compute accuracy
+
+    model.train()
+